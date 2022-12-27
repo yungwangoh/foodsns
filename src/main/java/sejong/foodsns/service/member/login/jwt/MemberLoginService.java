699f9de4f9f15bcdf8a51a21e271b8eb743b9c44
swager253 <swager253@naver.com>
2022-12-27 23:59:32 +0900

    controller 구현
    feat : 회원 CRUD 컨트롤러 구현 완료 및 로그인 컨트롤러 구현 완

--- src/main/java/sejong/foodsns/service/member/login/jwt/MemberLoginService.java
+++ src/main/java/sejong/foodsns/service/member/login/jwt/MemberLoginService.java
@@ -4,9 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
 import sejong.foodsns.dto.member.login.MemberLoginDto;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 public interface MemberLoginService {
     ResponseEntity<String> jwtLogin(MemberLoginDto loginDto) throws JsonProcessingException;
     ResponseEntity<String> jwtLogout(String email, String accessToken);
