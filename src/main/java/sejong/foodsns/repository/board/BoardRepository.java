4ed0d98aa9ea81457e0fdbd861e248674eafc5d0
Hayoon <hy97@sju.ac.kr>
2023-01-05 20:36:45 +0900

    feat: 제목으로 게시물 검색, 랭크 별 게시물 검색 메서드 구현

--- src/main/java/sejong/foodsns/repository/board/BoardRepository.java
+++ src/main/java/sejong/foodsns/repository/board/BoardRepository.java
@@ -3,7 +3,16 @@ package sejong.foodsns.repository.board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sejong.foodsns.domain.board.Board;
+import sejong.foodsns.domain.member.MemberRank;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+//    Optional<Board> findBoardByUsername(String name); // query dsl
+    Optional<Board> findBoardByTitle(String title);
+
+    List<Board> findBoardByMemberRank(MemberRank memberRank); // 랭크별 게시물 조회
 }
