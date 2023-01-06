1b5012c50dc3370247467045e973e37fbc974acb
Hayoon <hy97@sju.ac.kr>
2023-01-06 22:05:07 +0900

    feat: 게시물 제목 유무확인 메서드 추가

--- src/main/java/sejong/foodsns/repository/board/BoardRepository.java
+++ src/main/java/sejong/foodsns/repository/board/BoardRepository.java
@@ -3,6 +3,7 @@ package sejong.foodsns.repository.board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sejong.foodsns.domain.board.Board;
+import sejong.foodsns.domain.member.Member;
 import sejong.foodsns.domain.member.MemberRank;
 
 import java.util.List;
@@ -13,6 +14,9 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
 //    Optional<Board> findBoardByUsername(String name); // query dsl
     Optional<Board> findBoardByTitle(String title);
-
     List<Board> findBoardByMemberRank(MemberRank memberRank); // 랭크별 게시물 조회
+    Boolean existsBoardByTitle(String title);
+    void deleteBoardByMemberEmailAndMemberPassword(String email, String password);
+
+
 }
