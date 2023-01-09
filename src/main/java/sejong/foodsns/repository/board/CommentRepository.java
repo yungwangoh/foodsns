e09fa9ddc0058d068e3ff13ead9f0e1bf5d321c9
Hayoon <hy97@sju.ac.kr>
2023-01-09 19:27:38 +0900

    feat(CommentRepository): findCommentBy 메서드

--- src/main/java/sejong/foodsns/repository/board/CommentRepository.java
+++ src/main/java/sejong/foodsns/repository/board/CommentRepository.java
@@ -2,8 +2,14 @@ package sejong.foodsns.repository.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import sejong.foodsns.domain.board.Board;
 import sejong.foodsns.domain.board.Comment;
+import sejong.foodsns.domain.member.MemberRank;
+
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findCommentBy(MemberRank memberRank); // 랭크별 게시물 조회
+
 }
