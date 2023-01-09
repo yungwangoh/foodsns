2356722d1605e0768afe3bd3add794c9fdc42ca8
Hayoon <hy97@sju.ac.kr>
2023-01-09 19:28:31 +0900

    feat(NoSearchBoardException): Board 커스텀 예외처리

---
+++ src/main/java/sejong/foodsns/exception/http/NoSearchBoardException.java
@@ -0,0 +1,18 @@
+package sejong.foodsns.exception.http;
+
+public class NoSearchBoardException extends IllegalArgumentException {
+
+    public NoSearchBoardException() {}
+
+    public NoSearchBoardException(String s) {
+        super(s);
+    }
+
+    public NoSearchBoardException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoSearchBoardException(Throwable cause) {
+        super(cause);
+    }
+}
