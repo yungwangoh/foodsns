ce8548dac9dd8cc1cc8afa14a33d010b64b1b927
swager253 <swager253@naver.com>
2023-01-10 22:51:07 +0900

    친구 추가 서비스 구현 중 현재 테스트 진행

--- src/main/java/sejong/foodsns/domain/member/BlackList.java
+++ src/main/java/sejong/foodsns/domain/member/BlackList.java
@@ -28,7 +28,7 @@ public class BlackList extends BaseEntity {
     private String reason;
 
     // 신고 당한 회원
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "report_id")
     private ReportMember reportMember;
 
--- src/main/java/sejong/foodsns/repository/member/FriendRepository.java
+++ src/main/java/sejong/foodsns/repository/member/FriendRepository.java
@@ -3,7 +3,12 @@ package sejong.foodsns.repository.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sejong.foodsns.domain.member.Friend;
+import sejong.foodsns.domain.member.Member;
+
+import java.util.Optional;
 
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+
+    Optional<Friend> findByMember_Email(String email);
 }
