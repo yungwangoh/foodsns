57417490a96ed5f9ab3e72da53d6dc0498e7f1b5
Hayoon <hy97@sju.ac.kr>
2023-01-13 19:12:04 +0900

    feat: CommentRepository JpaRepo로 생성

--- src/main/java/sejong/foodsns/repository/board/CommentRepository.java
+++ src/main/java/sejong/foodsns/repository/board/CommentRepository.java
@@ -1,15 +1,26 @@
 package sejong.foodsns.repository.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sejong.foodsns.domain.board.Board;
 import sejong.foodsns.domain.board.Comment;
 import sejong.foodsns.domain.member.MemberRank;
 
+import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findCommentBy(MemberRank memberRank); // 랭크별 게시물 조회
+    //    Optional<Board> findBoardByUsername(String name); // query dsl
+    @Query("select c from Comment c join fetch c.board b where b.title = :title and c.content like %:content%")
+    Optional<Comment> findByBoardTitleAndContainingContent(@Param("title") String boardTitle, @Param("content") String content);
 
+    List<Comment> findByContentLike(String content);
+
+    Optional<Comment> findCommentById(Long id);
+
+    Boolean existsCommentByContentLike(String content);
 }
