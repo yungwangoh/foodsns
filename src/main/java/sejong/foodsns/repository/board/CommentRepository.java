4d9056fc616d38fd7f9984901b6d07fe4dfbdbd8
Hayoon <hy97@sju.ac.kr>
2023-01-15 17:50:15 +0900

    feat(CommentRepository): 게시물 회원이름 / 게시물제목으로 댓글 검색 쿼리문 구현

--- src/main/java/sejong/foodsns/repository/board/CommentRepository.java
+++ src/main/java/sejong/foodsns/repository/board/CommentRepository.java
@@ -4,18 +4,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import sejong.foodsns.domain.board.Board;
 import sejong.foodsns.domain.board.Comment;
-import sejong.foodsns.domain.member.MemberRank;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    //    Optional<Board> findBoardByUsername(String name); // query dsl
-    @Query("select c from Comment c join fetch c.board b where b.title = :title and c.content like %:content%")
+
+    @Query(value = "select c from Comment c join fetch c.board b join fetch b.member m where m.username = :username")
+    List<Comment> findCommentsByUsername(@Param("username") String username);
+
+    @Query(value = "select c from Comment c join fetch c.board b where b.title =:title")
+    List<Comment> findCommentsByBoardTitle(@Param("title") String title);
+
+    @Query(value = "select c from Comment c join fetch c.board b where b.title = :title and c.content like %:content%")
     Optional<Comment> findByBoardTitleAndContainingContent(@Param("title") String boardTitle, @Param("content") String content);
 
     List<Comment> findByContentLike(String content);
