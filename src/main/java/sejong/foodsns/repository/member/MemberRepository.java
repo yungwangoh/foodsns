0e21b2f10f7ec05dfac5773a260dd9a6025cd92c
Hayoon <hy97@sju.ac.kr>
2023-01-19 22:35:29 +0900

    수정

--- src/main/java/sejong/foodsns/repository/member/MemberRepository.java
+++ src/main/java/sejong/foodsns/repository/member/MemberRepository.java
@@ -3,6 +3,7 @@ package sejong.foodsns.repository.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 import sejong.foodsns.domain.member.Member;
 
@@ -11,6 +12,7 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+//    @Query("select m from Member m join fetch m.friends f where m.username = :username")
     Optional<Member> findMemberByUsername(String username);
     Optional<Member> findMemberByEmail(String email);
     @Query("select m from Member m join fetch m.boards b where b.id = :boardId")
