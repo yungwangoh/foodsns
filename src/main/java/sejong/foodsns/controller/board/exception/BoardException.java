89853e1d87eb93afb0dfc312ced7dd85d6348644
Hayoon <hy97@sju.ac.kr>
2023-01-19 17:19:18 +0900

    feat(BoardException): controller 예외 handler 구현

--- src/main/java/sejong/foodsns/controller/board/exception/BoardException.java
+++ src/main/java/sejong/foodsns/controller/board/exception/BoardException.java
@@ -1,6 +1,7 @@
 package sejong.foodsns.controller.board.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,5 +16,20 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @Slf4j
 @RestControllerAdvice("sejong.foodsns.controller.board")
 public class BoardException {
+    /**
+     * 중복된 제목 예외 처리
+     * @param e
+     * @return 잘못된 요청 400, Message
+     */
+    @ExceptionHandler
+    @ResponseStatus(BAD_REQUEST)
+    public ResponseEntity<ErrorResult> duplicatedExceptionHandler(DuplicatedException e) {
+        log.info("[duplicated exception]", e);
+        return getErrorResultResponseEntity(BAD_REQUEST, e);
+    }
 
+    private static ResponseEntity<ErrorResult> getErrorResultResponseEntity(HttpStatus status, Exception exception) {
+        ErrorResult errorResult = new ErrorResult(status, exception.getMessage());
+        return new ResponseEntity<>(errorResult, status);
+    }
 }
