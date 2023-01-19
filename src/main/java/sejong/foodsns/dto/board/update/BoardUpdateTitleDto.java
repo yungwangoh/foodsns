b4322b13038a6a1bcc3bd6628a64793d802c08ab
Hayoon <hy97@sju.ac.kr>
2023-01-19 22:34:39 +0900

    feat: Lombok, Builder, ArgsConstructor 어노테이션 Dto에 추가

--- src/main/java/sejong/foodsns/dto/board/update/BoardUpdateTitleDto.java
+++ src/main/java/sejong/foodsns/dto/board/update/BoardUpdateTitleDto.java
@@ -1,17 +1,16 @@
 package sejong.foodsns.dto.board.update;
 
-import lombok.Getter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
-@Getter
+@Data
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class BoardUpdateTitleDto {
 
-    @NotBlank
     private String orderTitle;
-
-    @NotBlank
-    @Pattern(regexp = "^{1,1000}$", message = "글자 제한 수는 50자 이내 입니다.")
     private String updateTitle;
 }
