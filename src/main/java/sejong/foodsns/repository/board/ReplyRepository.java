1c5dbc0a36fea726a5c2453097d766ff03565899
Hayoon <hy97@sju.ac.kr>
2023-02-06 19:08:09 +0900

    feat: ReplyRepository 생성

--- src/main/java/sejong/foodsns/repository/board/ReplyRepository.java
+++ src/main/java/sejong/foodsns/repository/board/ReplyRepository.java
@@ -1,9 +1,30 @@
 package sejong.foodsns.repository.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import sejong.foodsns.domain.board.Comment;
 import sejong.foodsns.domain.board.Reply;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    @Query(value = "select r from Reply r join fetch r.comment.board b join fetch b.member m where m.username = :username")
+    List<Reply> findRepliesByUsername(@Param("username") String username);
+
+    @Query(value = "select r from Reply r join fetch r.comment.board b where b.title =:title")
+    List<Reply> findRepliesByBoardTitle(@Param("title") String title);
+
+    @Query(value = "select r from Reply r join fetch r.comment.board b where b.title = :title and r.content like %:content%")
+    Optional<Reply> findByBoardTitleAndContainingContent(@Param("title") String boardTitle, @Param("content") String content);
+
+    List<Reply> findByContentLike(String content);
+
+    Optional<Reply> findReplyById(Long id);
+
+    Boolean existsReplyByContentLike(String content);
 }
