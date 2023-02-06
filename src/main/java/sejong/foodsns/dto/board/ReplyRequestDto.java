395bdcfc2e7ec3c960f0a259a5a1538fc4fbe459
Hayoon <hy97@sju.ac.kr>
2023-02-06 19:08:44 +0900

    feat: ReplyDto 생성

---
+++ src/main/java/sejong/foodsns/dto/board/ReplyRequestDto.java
@@ -0,0 +1,32 @@
+package sejong.foodsns.dto.board;
+
+import lombok.*;
+import sejong.foodsns.domain.board.Comment;
+import sejong.foodsns.domain.board.Reply;
+import sejong.foodsns.dto.member.MemberRequestDto;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ReplyRequestDto {
+
+    private Long id;
+    private String content;
+    private MemberRequestDto memberRequestDto;
+    private int recommCount;
+    private int reportCount;
+    private CommentRequestDto commentRequestDto;
+
+    public Reply toEntity() {
+        return Reply.builder()
+                .content(content)
+                .recommCount(recommCount)
+                .reportCount(reportCount)
+                .comment(commentRequestDto.toEntity())
+                .build();
+    }
+}
