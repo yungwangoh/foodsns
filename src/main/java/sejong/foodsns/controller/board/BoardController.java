af22b3bc5453b4e6e52c1379c574a874dd790fb9
Hayoon <hy97@sju.ac.kr>
2023-02-19 18:51:43 +0900

    feat: 게시물 Controller boardFile 관련 서비스 API 추가

--- src/main/java/sejong/foodsns/controller/board/BoardController.java
+++ src/main/java/sejong/foodsns/controller/board/BoardController.java
@@ -2,15 +2,24 @@ package sejong.foodsns.controller.board;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriUtils;
+import sejong.foodsns.domain.file.BoardFileType;
+import sejong.foodsns.domain.file.util.BoardFileStorage;
 import sejong.foodsns.dto.board.BoardRequestDto;
 import sejong.foodsns.dto.board.BoardResponseDto;
 import sejong.foodsns.dto.board.update.BoardUpdateTitleDto;
 import sejong.foodsns.service.board.crud.BoardCrudService;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,14 +33,16 @@ import static sejong.foodsns.service.board.crud.message.BoardSuccessOrFailedMess
 public class BoardController {
 
     private final BoardCrudService boardCrudService;
+    private final BoardFileStorage boardFileStorage;
 
     /**
      * 게시물 등록
+     *
      * @param boardRequestDto
      * @return 게시물, CREATE
      */
     @PostMapping("/board")
-    public ResponseEntity<BoardResponseDto> boardCreate(@RequestBody @Valid BoardRequestDto boardRequestDto) {
+    public ResponseEntity<BoardResponseDto> boardCreate(@RequestBody @Valid BoardRequestDto boardRequestDto) throws IOException {
         ResponseEntity<Optional<BoardResponseDto>> boardCreate = boardCrudService.boardCreate(boardRequestDto);
 
         return new ResponseEntity<>(getBoard(boardCreate), boardCreate.getStatusCode());
@@ -39,6 +50,7 @@ public class BoardController {
 
     /**
      * 게시물 검색
+     *
      * @param title
      * @return 회원 정보, OK
      */
@@ -52,6 +64,7 @@ public class BoardController {
 
     /**
      * 게시물 제목 수정
+     *
      * @param boardUpdateTitleDto
      * @return 게시물 제목 수정 완료, OK
      */
@@ -66,6 +79,7 @@ public class BoardController {
 
     /**
      * 게시물 목록 조회
+     *
      * @return 회원 목록, OK
      */
     @GetMapping("/boards")
@@ -78,6 +92,7 @@ public class BoardController {
 
     /**
      * 게시물 삭제
+     *
      * @param boardRequestDto
      * @return 게시물 삭제 완료, OK
      */
@@ -91,6 +106,7 @@ public class BoardController {
 
     /**
      * 게시물 제목 중복 검사
+     *
      * @param boardRequestDto
      * @return 중복을 찾는데에 성공하면 True 와 OK, 실패하면 False 와 NOT_FOUND
      */
@@ -99,7 +115,7 @@ public class BoardController {
 
         Boolean titleExistValidation = boardCrudService.boardTitleExistValidation(boardRequestDto);
 
-        if(titleExistValidation) {
+        if (titleExistValidation) {
             return new ResponseEntity<>(titleExistValidation.toString(), OK);
         } else {
             return new ResponseEntity<>(titleExistValidation.toString(), NOT_FOUND);
@@ -108,6 +124,7 @@ public class BoardController {
 
     /**
      * 게시물 목록 Optional Wrapping 해제 후 반환
+     *
      * @param boardList
      * @return 게시물 목록
      */
@@ -117,6 +134,7 @@ public class BoardController {
 
     /**
      * 게시물 Dto Optional Wrapping 해제 후 반환
+     *
      * @param board
      * @return 게시물 응답 Dto
      */
@@ -126,11 +144,30 @@ public class BoardController {
 
     /**
      * ResponseEntity Wrapping 해제 후 반환
+     *
      * @param boardCreate
      * @return Optionally Wrapped 댓글 응답 Dto
      */
     private Optional<BoardResponseDto> getBody(ResponseEntity<Optional<BoardResponseDto>> boardCreate) {
         return boardCreate.getBody();
+    }
+
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource processImg(@PathVariable String filename) throws MalformedURLException {
+        return new UrlResource("file: " + boardFileStorage.createPath(filename, BoardFileType.IMAGE));
+    }
+
+    @GetMapping("/boardFiles/{filename}")
+    public ResponseEntity<Resource> processAttaches(@PathVariable String filename, @RequestParam String originName) throws MalformedURLException {
+        UrlResource urlResource = new UrlResource("file:" + boardFileStorage.createPath(filename, BoardFileType.GENERAL));
+
+        String encodedUploadFileName = UriUtils.encode(originName, StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(urlResource);
     }
 }
 
