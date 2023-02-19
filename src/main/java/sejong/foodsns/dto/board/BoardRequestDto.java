c78953e372ddfd3fd3d01a9b385b7fca99f1ca0a
Hayoon <hy97@sju.ac.kr>
2023-02-19 18:54:22 +0900

    feat: 게시물 Dto 첨부파일 변수 추가로 인한 수정 및 구현

--- src/main/java/sejong/foodsns/dto/board/BoardRequestDto.java
+++ src/main/java/sejong/foodsns/dto/board/BoardRequestDto.java
@@ -1,14 +1,19 @@
 package sejong.foodsns.dto.board;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 import sejong.foodsns.domain.board.Board;
 import sejong.foodsns.domain.file.BoardFile;
+import sejong.foodsns.domain.file.BoardFileType;
 import sejong.foodsns.domain.member.Member;
 import sejong.foodsns.domain.member.MemberRank;
 import sejong.foodsns.dto.member.MemberRequestDto;
 
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Data
 @Builder
@@ -17,20 +22,22 @@ import java.util.List;
 public class BoardRequestDto {
 
     private Long id;
-
     private String title;
-
     private String content;
-
     private MemberRequestDto memberRequestDto;
-
     private Long check;
-
     private int recommCount;
+    private Map<BoardFileType, List<MultipartFile>> boardFiles;
 
-    private List<BoardFile> boardFiles;
+    @Builder
+    public BoardRequestDto(String title, String content, Map<BoardFileType, List<MultipartFile>> boardFiles) {
+        this.title = title;
+        this.content = content;
+        this.boardFiles = boardFiles;
+    }
 
     public Board toEntity() {
+
         return Board.builder()
                 .title(title)
                 .content(content)
@@ -38,6 +45,7 @@ public class BoardRequestDto {
                 .check(0L)
                 .recommCount(0)
                 .member(memberRequestDto.toEntity())
+                .boardFiles(new ArrayList<>())
                 .build();
     }
 }
